9c27739bcf10392afd4ea9d987b4a9f4b697e14f
Garethgareth26 <garethmuammar@gmail.com>
2026-07-09 16:49:21 +0700

    fix: app.php syntax error for vercel storage path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -19,6 +19,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })
-    ->useStoragePath(isset($_ENV['VERCEL']) ? '/tmp/storage' : storage_path())
-    ->create();
+    })->create();
+
+if (isset($_ENV['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
